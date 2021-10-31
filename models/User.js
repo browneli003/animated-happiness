2d5968a8ff0a5a127312c1d8df474843b15c92ee
Elia Brown <brownelijah143@gmail.com>
2021-10-30 19:23:59 -0600

    controllers done

---
+++ models/User.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true, 
+            required: 'Please provide a username!',
+            trim: true
+        },
+        email: {
+            type: String,
+            required: 'Please enter an email!',
+            unique: true,
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }    
+);
+
+// gets friend count
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('User', UserSchema)
+
+module.exports = User;
